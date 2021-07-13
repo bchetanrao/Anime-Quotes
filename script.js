df6760dcba11f5000051d40ed021bf13e0674e79
bchetanrao <chetanraocr7@gmail.com>
2021-07-13 18:01:19 +0530

    added files to this repo

---
+++ script.js
@@ -0,0 +1,12 @@
+// fetch('https://animechan.vercel.app/api/random')
+//         .then(response => response.json())
+//         .then(quote => {
+//             console.log(quote);
+//             document.getElementById("anime").innerHTML = quote.anime;
+//             document.getElementById("quote").innerHTML = quote.quote;
+//             document.getElementById("character").innerHTML = quote.character;
+//         })
+
+
+
+
