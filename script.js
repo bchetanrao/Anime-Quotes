4e8e0ef26320f84f1a9bcf50f8b77e3445701a6c
bchetanrao <chetanraocr7@gmail.com>
2021-07-13 18:10:59 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// fetch('https://animechan.vercel.app/api/random')
-//         .then(response => response.json())
-//         .then(quote => {
-//             console.log(quote);
-//             document.getElementById("anime").innerHTML = quote.anime;
-//             document.getElementById("quote").innerHTML = quote.quote;
-//             document.getElementById("character").innerHTML = quote.character;
-//         })
-
-
-
-
+fetch('https://animechan.vercel.app/api/random')
+        .then(response => response.json())
+        .then(quote => {
+            console.log(quote);
+            document.getElementById("anime").innerHTML = quote.anime;
+            document.getElementById("quote").innerHTML = quote.quote;
+            document.getElementById("character").innerHTML = quote.character;
+        })
